563e94ef081919e2120ab80c1deb3bd71a263e18
Michael Tillman <mnamllit@gmail.com>
2022-07-09 21:39:03 -0500

    Edit server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const serveStatic = require("serve-static")
+const history = require('connect-history-api-fallback');
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history(
+    {index: '/'}
+));
 const port = process.env.PORT || 3000;
 app.listen(port);
