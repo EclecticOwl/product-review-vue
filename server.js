7ee4d3cbfd770e2d656a535297339cda20f5c769
Michael Tillman <mnamllit@gmail.com>
2022-07-09 21:58:18 -0500

    .

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var app = express();
+const serveStatic = require("serve-static")
+const path = require('path');
 var history = require('connect-history-api-fallback');
+let app = express();
+
 
 app.use(history({
 
 }));
 
-app.use(express.static(__dirname));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.set('port', (process.env.PORT || 8080));
 
