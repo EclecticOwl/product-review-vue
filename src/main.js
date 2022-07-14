5e5a06702fa3f7535e2b10ed7ab360c31897e815
Michael Tillman <mnamllit@gmail.com>
2022-07-14 02:53:12 -0500

    Changed back baseurl

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://lit-reaches-65994.herokuapp.com'
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'https://lit-reaches-65994.herokuapp.com'
 
 createApp(App).use(store).use(router, axios).mount('#app')
